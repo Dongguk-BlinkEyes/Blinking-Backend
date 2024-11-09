49572bcf313e30747d86f728d5542d681a6eeb64
jjjinsung <pjs0625@dgu.ac.kr>
2024-11-09 18:53:59 +0900

    first commit

---
+++ src/test/java/Dongguk/Blinking_AI/BlinkingAiApplicationTests.java
@@ -0,0 +1,13 @@
+package Dongguk.Blinking_AI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlinkingAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
